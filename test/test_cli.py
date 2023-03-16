5e1260956bdb39e84b7f9211362434dc5428604e
Zeevo <8338013+zeevo@users.noreply.github.com>
2023-03-16 18:42:57 -0400

    Fix tests

--- test/test_cli.py
+++ test/test_cli.py
@@ -1,9 +1,10 @@
-from cli import cli
 from click.testing import CliRunner
+
+from src.cli import cli
 
 
 def test_example():
     runner = CliRunner()
-    result = runner.invoke(cli, ["example"])
+    result = runner.invoke(cli, ["hello"])
     assert result.exit_code == 0
     assert result.output == "Hello world.\n"
